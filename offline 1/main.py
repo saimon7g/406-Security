08ab3b4a245a6331270c30a5d95713235d7824a0
Nur Uddin Ibne Huda <nuruddin.simon@gmail.com>
2023-11-30 16:35:40 +0600

    Decription completed

--- offline 1/main.py
+++ offline 1/main.py
@@ -18,12 +18,38 @@ Sbox = (
     [0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16]
 )
 
+InvSbox = (
+    [0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB],
+    [0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB],
+    [0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E],
+    [0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25],
+    [0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92],
+    [0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84],
+    [0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06],
+    [0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B],
+    [0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73],
+    [0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E],
+    [0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B],
+    [0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4],
+    [0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F],
+    [0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF],
+    [0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61],
+    [0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D]
+)
+
 Mixer = [
     [BitVector(hexstring="02"), BitVector(hexstring="03"), BitVector(hexstring="01"), BitVector(hexstring="01")],
     [BitVector(hexstring="01"), BitVector(hexstring="02"), BitVector(hexstring="03"), BitVector(hexstring="01")],
     [BitVector(hexstring="01"), BitVector(hexstring="01"), BitVector(hexstring="02"), BitVector(hexstring="03")],
     [BitVector(hexstring="03"), BitVector(hexstring="01"), BitVector(hexstring="01"), BitVector(hexstring="02")]
 ]
+InvMixer = [
+    [BitVector(hexstring="0E"), BitVector(hexstring="0B"), BitVector(hexstring="0D"), BitVector(hexstring="09")],
+    [BitVector(hexstring="09"), BitVector(hexstring="0E"), BitVector(hexstring="0B"), BitVector(hexstring="0D")],
+    [BitVector(hexstring="0D"), BitVector(hexstring="09"), BitVector(hexstring="0E"), BitVector(hexstring="0B")],
+    [BitVector(hexstring="0B"), BitVector(hexstring="0D"), BitVector(hexstring="09"), BitVector(hexstring="0E")]
+]
+
 
 def substitute_word_Sbox(word):
     substituted_word = ""
@@ -41,6 +67,14 @@ def substitute_matriix_Sbox(matrix):
             matrix[i][j]=temp
     return matrix 
 
+def substitute_matriix_inverse_Sbox(matrix):
+    for i in range(0,4):
+        for j in range(0,4):
+            temp=InvSbox[int(matrix[i][j][0],16)][int(matrix[i][j][1],16)]
+            temp=format_hex(str(hex(temp))[2:])
+            matrix[i][j]=temp
+    return matrix
+
 def format_hex(hex_string):
     if len(hex_string) == 1:
         return "0" + hex_string
@@ -56,6 +90,12 @@ def convert_to_hex(string):
         temp=hex(ord(char))[2:]
         hex_string += format_hex(temp)
     return hex_string
+
+def convert_to_string(hex_string):
+    string = ""
+    for i in range(0,len(hex_string),2):
+        string+=chr(int(hex_string[i:i+2],16))
+    return string
 
 
 
@@ -90,6 +130,13 @@ def column_major_convert(string):
         # column major conversion
         matrix[int((i%8)/2)][int(i/8)]=string[i:i+2]
     return matrix
+
+def column_major_to_string(matrix):
+    string=""
+    for i in range(0,4):
+        for j in range(0,4):
+            string+=matrix[j][i]
+    return string
     
 
 def xOr_twoWords(word1,word2):
@@ -104,6 +151,7 @@ def xOR_two_matrix(matrix1,matrix2):
     for i in range(0,4):
         for j in range(0,4):
             temp=hex(int(matrix1[i][j],16) ^ int(matrix2[i][j],16))[2:]
+            temp+=""
             temp=format_hex(temp)
             matrix1[i][j]=temp
     return matrix1
@@ -125,6 +173,11 @@ def shift_rows(matrix):
     for i in range(0,4):
         matrix[i]=matrix[i][i:]+matrix[i][0:i]
     return matrix
+
+def inverse_shift_rows(matrix):
+    for i in range(0,4):
+        matrix[i]=matrix[i][4-i:]+matrix[i][0:4-i]
+    return matrix
 def mix_column(matrix):
     AES_modulus = BitVector(bitstring='100011011')
     result_matrix = [[0 for x in range(4)] for y in range(4)]
@@ -139,6 +192,25 @@ def mix_column(matrix):
                 ans=ans.__xor__(temp3)
             result_matrix[i][j]=ans.get_bitvector_in_hex()
     return result_matrix
+
+
+def inverse_mix_column(matrix):
+    AES_modulus = BitVector(bitstring='100011011')
+    result_matrix = [[0 for x in range(4)] for y in range(4)]
+    for i in range(0,4):
+        for j in range(0,4):
+            # galois multiplication
+            ans=BitVector(hexstring="00")
+            for k in range(0,4):
+                temp1=InvMixer[i][k]
+                temp2=BitVector(hexstring=matrix[k][j])
+                temp3=temp1.gf_multiply_modular(temp2,AES_modulus,8)
+                ans=ans.__xor__(temp3)
+            result_matrix[i][j]=ans.get_bitvector_in_hex()
+    return result_matrix
+
+
+
 
 
 
@@ -170,20 +242,24 @@ print("State after add round key: ",state)
 
 # rounds
 for i in range(0,10):
-    print("Round ",i+1,": ")
+    # print("Round ",i+1,": ")
     # substitute bytes
     state=substitute_matriix_Sbox(state)
-    print("State after substitute bytes: ",state)
+    # print("State after substitute bytes: ",state)
     # shift rows
     state=shift_rows(state)
-    print("State after shift rows: ",state)
+    # print("State after shift rows: ",state)
     # mix columns
     if i!=9:
         state=mix_column(state)
-    print("State after mix columns: ",state)
+    # print("State after mix columns: ",state)
     # add round key
     state=xOR_two_matrix(state,column_major_convert(roundKeys[i+1]))
-    print("State after add round key: ",state)
+    # print("State after add round key: ",state)
+
+cipherTextToRelay=column_major_to_string(state)
+print("Cipher Text: ",cipherTextToRelay)
+
 
 
 # # substitute bytes
@@ -198,6 +274,71 @@ for i in range(0,10):
 # # add round key
 # state=xOR_two_matrix(state,column_major_convert(roundKeys[1]))
 # print("State after add round key: ",state)
+
+
+
+# roundKeys = []
+# roundKeys.append(convertedHexKey[0:32])
+# previousRoundConstant=0x00
+# for i in range(0,10):
+#     previousRoundConstant = calculate_round_constant(previousRoundConstant)
+#     gOfInput=generate_g(convertedHexKey[24:32],previousRoundConstant)
+#     roundkey=generate_w_XOR(convertedHexKey[0:32],gOfInput)
+#     roundKeys.append(roundkey)
+#     convertedHexKey=roundkey
+
+
+
+secretKeyAtReceiverEnd = "Thats my Kung Fu"
+cipherTextReceived = cipherTextToRelay
+secretKeyHex = convert_to_hex(secretKeyAtReceiverEnd)
+
+roundKeysDecr = []
+roundKeysDecr.append(secretKeyHex)
+prevRoundConstant=0x00
+for i in range(0,10):
+    prevRoundConstant = calculate_round_constant(prevRoundConstant)
+    gOfW=generate_g(secretKeyHex[24:32],prevRoundConstant)
+    roundkeyR=generate_w_XOR(secretKeyHex,gOfW)
+    roundKeysDecr.append(roundkeyR)
+    secretKeyHex=roundkeyR
+    
+for i in range(0,len(roundKeysDecr)):
+    print("Round Key ",i,": ",roundKeysDecr[i])
+    
+    
+    
+    
+    
+cipherMatrix = column_major_convert(cipherTextReceived)
+cipherMatrix=xOR_two_matrix(cipherMatrix,column_major_convert(roundKeysDecr[10]))
+print("State after add round key: ",cipherMatrix)
+# rounds
+for i in range(9,0,-1):
+    print("Round ",i,": ")
+    # inverse shift rows
+    cipherMatrix=inverse_shift_rows(cipherMatrix)
+    print("State after inverse shift rows: ",cipherMatrix)
+    # inverse substitute bytes
+    cipherMatrix=substitute_matriix_inverse_Sbox(cipherMatrix)
+    print("State after inverse substitute bytes: ",cipherMatrix)
+    # add round key
+    cipherMatrix=xOR_two_matrix(cipherMatrix,column_major_convert(roundKeysDecr[i]))
+    print("State after add round key: ",cipherMatrix)
+    # inverse mix columns
+    cipherMatrix=inverse_mix_column(cipherMatrix)
+    print("State after inverse mix columns: ",cipherMatrix)
+    
+cipherMatrix=inverse_shift_rows(cipherMatrix)
+cipherMatrix=substitute_matriix_inverse_Sbox(cipherMatrix)
+cipherMatrix=xOR_two_matrix(cipherMatrix,column_major_convert(roundKeysDecr[0]))
+messageReceived = column_major_to_string(cipherMatrix)
+
+messageReceived = convert_to_string(messageReceived)
+print("Message Received: ",messageReceived)
+
+
+
 
 
 
